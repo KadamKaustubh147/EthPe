f35d86552f922eea4ee4a1887bd0df3e04b79df3
Ashking <Ashking21@proton.me>
2025-07-04 16:27:28 +0530

    Updated register route

---
+++ controllers/authControllers.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  try {
+    const {email, password } = req.body;
+
+    if (await User.findOne({ email })) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
+
+    const user = await User.create({
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.status(201).json({ message: 'Registered', token });
+  } catch (err) {
+    res.status(500).json({ error: 'Registration failed' });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token, email: user.email });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const protectedRoute = (req, res) => {
+  res.json({ message: "You are authenticated", user: req.user });
+};
+
+module.exports = { register, login, protectedRoute };
