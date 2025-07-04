55a538ab38d6e5e547adc93669cd450c2b901638
Ashking <Ashking21@proton.me>
2025-07-04 17:43:50 +0530

    added token to regstr and name in regstr

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,17 +3,24 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
   try {
-    const exists = await User.findOne({ email });
-    if (exists) return res.status(400).json({ message: "User already exists" });
+    const {name, email, password } = req.body;
 
-    const hashed = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashed });
+    if (await User.findOne({ email })) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
 
-    res.status(201).json({ message: "User created", user: newUser.email });
+    const user = await User.create({
+      name,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.status(201).json({ message: 'Registered', token });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: 'Registration failed' });
   }
 };
 
